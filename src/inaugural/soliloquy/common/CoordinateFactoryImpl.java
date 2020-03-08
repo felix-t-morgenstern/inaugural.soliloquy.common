66c3c166912a0ed4fd1378ba04c226589fc4ac13
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2020-03-08 00:48:42 -0500

    Test and implement hashCode, equals, and toString for CoordinateFactoryImpl
    Test and implement hashCode, equals, and toString for CoordinateFactoryImpl

--- src/inaugural/soliloquy/common/CoordinateFactoryImpl.java
+++ src/inaugural/soliloquy/common/CoordinateFactoryImpl.java
@@ -15,4 +15,19 @@ public class CoordinateFactoryImpl implements CoordinateFactory {
         return CoordinateFactory.class.getCanonicalName();
     }
 
+    @Override
+    public int hashCode() {
+        return CoordinateFactoryImpl.class.getCanonicalName().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof CoordinateFactoryImpl && obj.hashCode() == hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return CoordinateFactoryImpl.class.getCanonicalName();
+    }
+
 }
--- src/inaugural/soliloquy/common/test/CoordinateFactoryImplTests.java
+++ src/inaugural/soliloquy/common/test/CoordinateFactoryImplTests.java
@@ -1,5 +1,6 @@
 package inaugural.soliloquy.common.test;
 
+import inaugural.soliloquy.common.test.fakes.FakeCoordinateFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import inaugural.soliloquy.common.CoordinateFactoryImpl;
@@ -29,5 +30,28 @@ class CoordinateFactoryImplTests {
     void testGetInterfaceName() {
         assertEquals(CoordinateFactory.class.getCanonicalName(),
                 _coordinateFactory.getInterfaceName());
+    }
+
+    @Test
+    void testHashCode() {
+        assertEquals(CoordinateFactoryImpl.class.getCanonicalName().hashCode(),
+                _coordinateFactory.hashCode());
+    }
+
+    @SuppressWarnings({"SimplifiableJUnitAssertion", "ConstantConditions"})
+    @Test
+    void testEquals() {
+        CoordinateFactory equalCoordinateFactory = new CoordinateFactoryImpl();
+        CoordinateFactory unequalCoordinateFactory = new FakeCoordinateFactory();
+
+        assertTrue(_coordinateFactory.equals(equalCoordinateFactory));
+        assertFalse(_coordinateFactory.equals(unequalCoordinateFactory));
+        assertFalse(_coordinateFactory.equals(null));
+    }
+
+    @Test
+    void testToString() {
+        assertEquals(CoordinateFactoryImpl.class.getCanonicalName(),
+                _coordinateFactory.toString());
     }
 }
