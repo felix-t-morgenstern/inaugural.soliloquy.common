ee9cca3893775713aea99fe75708cc111083f707
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2025-07-19 03:54:34 -0400

    refactor: Define explicit DI interface, refactor away from Guice

--- src/main/java/inaugural/soliloquy/common/CommonModule.java
+++ src/main/java/inaugural/soliloquy/common/CommonModule.java
@@ -1,44 +1,47 @@
 package inaugural.soliloquy.common;
 
-import com.google.inject.AbstractModule;
 import inaugural.soliloquy.common.persistence.*;
-import soliloquy.specs.common.persistence.PersistenceHandler;
+import inaugural.soliloquy.tools.Check;
+import org.int4.dirk.api.Injector;
+import org.int4.dirk.di.Injectors;
+import soliloquy.specs.game.Module;
 
-public class CommonModule extends AbstractModule {
-    private final PersistenceHandler PERSISTENCE_HANDLER;
+public class CommonModule implements Module {
+    private final Injector INJECTOR;
 
-    // TODO: Refactor away from service location pattern when JDK 13 is released
-    // NB: JDK 12 results in "Illegal reflective access" errors thrown by Guice; a Google support
-    //     technician has stated that this error will require workarounds until JDK 13 is released,
-    //     but no workarounds (in that thread or elsewhere) have worked to resolve the issue.
-    //     (The issue is that refactoring to true DI instead of service location requires the new
-    //     injector to produce the PersistenceHandler, to manage the recursive dependencies
-    //     with MapHandler, CollectionHandler, etc. This may also be a code smell.)
-    //     [Source: https://github.com/google/guice/issues/1133]
     public CommonModule() {
-        PERSISTENCE_HANDLER = new PersistenceHandlerImpl();
+        var persistenceHandler = new PersistenceHandlerImpl();
 
         var booleanHandler = new BooleanHandler();
         var coordinate2dHandler = new Coordinate2dHandler();
         var coordinate3dHandler = new Coordinate3dHandler();
-        var uuidHandler = new UuidHandler();
         var integerHandler = new IntegerHandler();
         var stringHandler = new StringHandler();
-        var mapHandler = new MapHandler(PERSISTENCE_HANDLER);
-        var pairHandler = new PairHandler(PERSISTENCE_HANDLER);
+        var listHandler = new ListHandler(persistenceHandler);
+        var mapHandler = new MapHandler(persistenceHandler);
+        var pairHandler = new PairHandler(persistenceHandler);
 
-        PERSISTENCE_HANDLER.addTypeHandler(booleanHandler);
-        PERSISTENCE_HANDLER.addTypeHandler(coordinate2dHandler);
-        PERSISTENCE_HANDLER.addTypeHandler(coordinate3dHandler);
-        PERSISTENCE_HANDLER.addTypeHandler(uuidHandler);
-        PERSISTENCE_HANDLER.addTypeHandler(integerHandler);
-        PERSISTENCE_HANDLER.addTypeHandler(stringHandler);
-        PERSISTENCE_HANDLER.addTypeHandler(mapHandler);
-        PERSISTENCE_HANDLER.addTypeHandler(pairHandler);
+        persistenceHandler.addTypeHandler(booleanHandler);
+        persistenceHandler.addTypeHandler(coordinate2dHandler);
+        persistenceHandler.addTypeHandler(coordinate3dHandler);
+        persistenceHandler.addTypeHandler(integerHandler);
+        persistenceHandler.addTypeHandler(stringHandler);
+        persistenceHandler.addTypeHandler(listHandler);
+        persistenceHandler.addTypeHandler(mapHandler);
+        persistenceHandler.addTypeHandler(pairHandler);
+
+        INJECTOR = Injectors.manual();
+
+        INJECTOR.registerInstance(persistenceHandler);
     }
 
     @Override
-    protected void configure() {
-        bind(PersistenceHandler.class).toInstance(PERSISTENCE_HANDLER);
+    public <T> T provide(Class<T> clazz) throws IllegalArgumentException {
+        return INJECTOR.getInstance(clazz);
+    }
+
+    public <T> T provide(String instance) throws IllegalArgumentException {
+        Check.ifNullOrEmpty(instance, "instance");
+        throw new IllegalArgumentException("No named instances within CommonModule");
     }
 }
---
+++ src/test/java/inaugural/soliloquy/common/test/unit/CommonModuleTests.java
@@ -0,0 +1,57 @@
+package inaugural.soliloquy.common.test.unit;
+
+import inaugural.soliloquy.common.CommonModule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import soliloquy.specs.common.persistence.PersistenceHandler;
+
+import java.util.Map;
+
+import static inaugural.soliloquy.tools.collections.Collections.mapOf;
+import static inaugural.soliloquy.tools.random.Random.randomInt;
+import static inaugural.soliloquy.tools.random.Random.randomString;
+import static org.junit.jupiter.api.Assertions.*;
+import static soliloquy.specs.common.valueobjects.Pair.pairOf;
+
+@ExtendWith(MockitoExtension.class)
+public class CommonModuleTests {
+    // (NB: Taken from MapHandlerTests to increase surety that instance works properly)
+    private final String KEY_1 = randomString();
+    private final String KEY_2 = randomString();
+    private final Integer VALUE_1 = randomInt();
+    private final Integer VALUE_2 = randomInt();
+    private final String MAP_STRING = String.format(
+            "{\"keyType\":\"%s\",\"valueType\":\"%s\",\"keys\":[\"%s\",\"%s\"]," +
+                    "\"values\":[\"%d\",\"%d\"]}",
+            String.class.getCanonicalName(), Integer.class.getCanonicalName(),
+            KEY_1, KEY_2, VALUE_1, VALUE_2);
+
+    private CommonModule module;
+
+    @BeforeEach
+    public void setUp() {
+        module = new CommonModule();
+    }
+
+    @Test
+    public void testProvideClass() {
+        var persistenceHandler = module.provide(PersistenceHandler.class);
+        var mapHandler = persistenceHandler.getTypeHandler(Map.class.getCanonicalName());
+        var map = mapOf(
+                pairOf(KEY_1, VALUE_1),
+                pairOf(KEY_2, VALUE_2)
+        );
+        var mapHandlerOutput = mapHandler.write(map);
+
+        assertNotNull(persistenceHandler);
+        assertEquals(MAP_STRING, mapHandlerOutput);
+        assertEquals(map, mapHandler.read(MAP_STRING));
+    }
+
+    @Test
+    public void testProvideInstance() {
+        assertThrows(IllegalArgumentException.class, () -> module.provide(randomString()));
+    }
+}
